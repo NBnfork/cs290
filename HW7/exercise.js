a29425bbcb6fc4c76242d99d4c51ba09170feebb
Noah <buchenn@oregonstate.edu>
2018-06-10 10:17:13 -0400

    changed port added db connect

--- HW7/exercise.js
+++ HW7/exercise.js
@@ -1,3 +1,13 @@
+//database connection
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host  : 'localhost',
+    user  : 'student',
+    password: 'default',
+    database: 'student'
+});
+
+
 var express = require('express');
 
 var app = express();
@@ -6,8 +16,9 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 6556);
+app.set('port', 10001);
 
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
     res.render('home.handlebars')
@@ -28,4 +39,38 @@ app.use(function(err, req, res, next){
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+//database set-up
+var mysql = require('mysql');
+var pool = mysql.createPool();
+
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+        pool.query("DROP TABLE IF EXISTS workouts", function(err){
+        var createString = "CREATE TABLE workouts("+
+            "id INT PRIMARY KEY AUTO_INCREMENT,"+
+            "" +
+            "ame VARCHAR(255) NOT NULL,"+
+            "reps INT,"+
+            "weight INT,"+
+            "date DATE,"+
+            "lbs BOOLEAN)";
+        pool.query(createString, function(err){
+            context.results = "Table reset";
+            res.render('home',context);
+        })
+    });
+});
+
+app.get('/insert',function(req,res,next){
+    var context = {};
+    mysql.pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.c], function(err, result){
+        if(err){
+            next(err);
+            return;
+        }
+        context.results = "Inserted id " + result.insertId;
+        res.render('home',context);
+    });
 });
