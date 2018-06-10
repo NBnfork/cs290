d8a83fb81c81803edb654d79c22c52fc8b02d8e8
Noah <buchenn@oregonstate.edu>
2018-06-10 14:59:40 -0400

    db testing

--- DBpractice/dbcon.js
+++ DBpractice/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'mysql.eecs.oregonstate.edu',
-  user            : 'cs290_buchenn',
-  password        : '6556',
-  database        : 'cs290_buchenn'
+  user            : 'buchenn',
+  password        : 'CMB!!Os4ntb',
+  database        : 'buchenn'
 });
 
 module.exports.pool = pool;
