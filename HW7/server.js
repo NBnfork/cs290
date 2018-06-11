c0715c53af59667762f924c8c9aeb6ad433b9ebf
Noah <buchenn@oregonstate.edu>
2018-06-11 02:20:17 -0400

    got mysql

--- HW7/server.js
+++ HW7/server.js
@@ -2,11 +2,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host  : 'classmysql.engr.oregonstate.edu',
+    host  : 'mysql.eecs.oregonstate.edu',
     user  : 'cs290_buchenn',
     password: '6556',
     database: 'cs290_buchenn',
-    acquireTimeout: 200
 });
 
 var express = require('express');
